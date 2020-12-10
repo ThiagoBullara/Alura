3cc8b35e511a0d1fa4a61f9052ab21fcdd139d50
Thiago Leite <thibullara99@gmail.com>
2020-12-10 16:48:26 -0300

    for

--- Front-End/Javascript/JavaScript-Navegador1/js/index.js
+++ Front-End/Javascript/JavaScript-Navegador1/js/index.js
@@ -2,35 +2,40 @@ let h1 = document.querySelector(".titulo"); //h1 ou #titulo
 h1.textContent = "Aparecida Nutricionista";
 
 // Elementos
-var tabela = document.querySelector("#tabela-pacientes");
-var paciente = tabela.querySelector(".paciente");
-var pesoTb = paciente.querySelector(".info-peso");
-var alturaTb = paciente.querySelector(".info-altura");
-var imcTd = document.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
 
-// Valores dos elementos
-let peso = pesoTb.textContent;
-let altura = alturaTb.textContent;
+for (let i = 0; i < pacientes.length; i++) {
 
-// Validação de valores
-var validacaoPeso = (peso >= 2 && peso <= 300);
-var validacaoAltura = (altura >= 0.5 && altura < 2.5);
-var validacao = false;
+    // Valores dos elementos
+    var pesoTD = pacientes[i].querySelector(".info-peso");
+    var alturaTD = pacientes[i].querySelector(".info-altura");
+    var imcTD = pacientes[i].querySelector(".info-imc");
+    var peso = pesoTD.textContent;
+    var altura = alturaTD.textContent;
 
-if (!validacaoPeso) {
-    pesoTb.textContent = "Peso inválido";
-};
+    // Validação de valores
+    var validacaoPeso = (peso >= 2 && peso <= 300);
+    var validacaoAltura = (altura >= 0.5 && altura < 2.5);
+    var imc = peso/(altura * altura);
+    var validacao = false;
 
-if (!validacaoAltura) {
-    alturaTb.textContent = "Altura inválida";
-};
+    if (!validacaoPeso) {
+        pesoTD.textContent = "Peso inválido";
+        pesoTD.classList.add("paciente-invalido");
+    };
 
-if (validacaoPeso && validacaoAltura) {
-    validacao = true;
-};
+    if (!validacaoAltura) {
+        alturaTD.textContent = "Altura inválida";
+        alturaTD.classList.add("paciente-invalido");
+    };
 
-if (validacao) {
-    imcTd.textContent = peso/(altura * altura);
-} else {
-    imcTd.textContent = "Error";
+    if (validacaoPeso && validacaoAltura) {
+        validacao = true;
+    };
+
+    if (validacao) {
+        imcTD.textContent = imc.toFixed(2);
+    } else {
+        imcTD.textContent = "Error";
+    };
 };
