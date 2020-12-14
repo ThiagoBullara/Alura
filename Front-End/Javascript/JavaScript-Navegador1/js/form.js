7a24307d3c460fae4af5c8d5179ad295816d3361
Thiago Leite <thibullara99@gmail.com>
2020-12-14 16:34:11 -0300

    atualizando form

--- Front-End/Javascript/JavaScript-Navegador1/js/form.js
+++ Front-End/Javascript/JavaScript-Navegador1/js/form.js
@@ -3,67 +3,78 @@ let addButton = document.querySelector("#adicionar-paciente")
 addButton.addEventListener("click", function(event) {
     event.preventDefault();
 
-    // Pega o formulário
     var form = document.querySelector("#formulario");
-
-    // Cria a estrutura do formulário
-    var pacienteTR = document.createElement("tr");
-    var nomeTD = document.createElement("td");
-    var pesoTD = document.createElement("td");
-    var alturaTD = document.createElement("td");
-    var gorduraTD = document.createElement("td");
-    var imcTD = document.createElement("td");
-
-    // Valores do formulário
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    // Validação
-    var validacaoPeso = (peso >= 2 && peso <= 300);
-    var validacaoAltura = (altura >= 0.5 && altura < 2.5);
-    var imc = peso/(altura * altura);
-    var validacao = false;
-
-    // Adiciona os valores nas respectivas estruturas
-    nomeTD.textContent = nome;
-    pesoTD.textContent = peso;
-    alturaTD.textContent = altura;
-    gorduraTD.textContent = gordura;
-    imcTD.textContent = imc;
-
-    if (!validacaoPeso) {
-        pesoTD.textContent = "Peso inválido";
-        pesoTD.classList.add("paciente-invalido");
-    };
-
-    if (!validacaoAltura) {
-        alturaTD.textContent = "Altura inválida";
-        alturaTD.classList.add("paciente-invalido");
-    };
-
-    if (validacaoPeso && validacaoAltura) {
-        validacao = true;
-    };
-
-    if (validacao) {
-        imcTD.textContent = imc.toFixed(2);
-    } else {
-        imcTD.textContent = "Error";
-    };
-
-    // Adiciona a estrutura na row
-    pacienteTR.appendChild(nomeTD);
-    pacienteTR.appendChild(pesoTD);
-    pacienteTR.appendChild(alturaTD);
-    pacienteTR.appendChild(gorduraTD);
-    pacienteTR.appendChild(imcTD);
-
-    // Pega a tabela
     var tabela = document.querySelector("#tabela-pacientes");
-
-    // Adiciona a estrutura na tabela
+    var paciente = valoresPaciente(form);
+    var pacienteTR = montarPaciente(paciente);
+    var validacaoPeso = validaPeso(paciente);
+    var validacaoAltura = validaAltura(paciente);
+    var validacaoFinalResultado = validacaoFinal(validacaoPeso, validacaoAltura);
     tabela.appendChild(pacienteTR);
+    form.reset();
 
+    console.log(validacaoFinalResultado);
+
+    function valoresPaciente(form) {
+
+        var paciente = {
+            nome: form.nome.value,
+            peso: form.peso.value,
+            altura: form.altura.value,
+            gordura: form.gordura.value,
+            imc: form.peso.value/(form.altura.value * form.altura.value)
+        };
+
+        return paciente;
+    };
+
+    function montarPaciente(paciente) {
+
+        var pacienteTR = document.createElement("tr");
+        pacienteTR.classList.add("paciente");
+
+        pacienteTR.appendChild(montaTD(paciente.nome, "info-nome"));
+        pacienteTR.appendChild(montaTD(paciente.peso, "info-peso"));
+        pacienteTR.appendChild(montaTD(paciente.altura, "info-altura"));
+        pacienteTR.appendChild(montaTD(paciente.gordura, "info-gordura"));
+        pacienteTR.appendChild(montaTD(paciente.imc.toFixed(2), "info-imc"));
+
+        return pacienteTR;
+    };
+
+    function montaTD(dado, classe) {
+        var td = document.createElement("td");
+        td.textContent = dado;
+        td.classList.add(classe);
+
+        return td;
+    };
+
+    function validaPeso(paciente) {
+        if(paciente.peso < 200 && paciente.peso >= 2) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    function validaAltura(paciente) {
+        if(paciente.altura < 2.3 && paciente.altura >= 0.3) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    function validacaoFinal(validacaoPeso, validacaoAltura) {
+        if(validacaoPeso && validacaoAltura) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    if(!validacaoFinalResultado) {
+        pacienteTR.classList.add("paciente-invalido");
+    };
 });
